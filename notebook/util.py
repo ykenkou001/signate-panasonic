397bbd6c2fa2368235cbc360e93a92362714e132
ykenko001 <ykenko001@gmail.com>
2021-05-30 13:28:28 +0000

    convert_to_coco_format関数の追加

--- notebook/util.py
+++ notebook/util.py
@@ -1,6 +1,17 @@
 import json
+import os
+from pathlib import Path
 
 import cv2
+import numpy as np
+
+test_images = sorted(Path('../data/test_images').glob('**/*.png'))
+train_images = sorted(Path('../data/train_images').glob('**/*.png'))
+train_annotations = sorted(
+    Path('../data/train_annotations/').glob('**/*.json'))
+
+rect_keys = ['引戸', '折戸', '開戸']
+polygon_keys = ['LDK', '廊下', '浴室']
 
 
 def read_json(json_path: str):
@@ -10,17 +21,138 @@ def read_json(json_path: str):
 
 
 def draw_rectangle(img, pts):
+    """"矩形を描写する
+
+    Args:
+        img (ndarray): cv2.imreadで読み込んだ画像
+        pts (list[int]):　座標
+
+    Returns:
+        [type]: imgに矩形を描写した画像
+    """
     return cv2.rectangle(img, tuple(pts[:2]), tuple(pts[2:]), (255, 0, 0), 2)
 
 
-def draw_polylines(img, pts):
+def draw_polylines(img, pts, rgb=(0, 0, 255)):
+    """画像情に多角形を描写する
+
+    Args:
+        img (np.ndarray): cv2.imreadで読み込んだ画像
+        pts (list(int)): 座標
+
+    Returns:
+        [np.ndarray]: imgに多角形を描写した画像
+    """
     pts = pts.reshape((-1, 1, 2))
-    return cv2.polylines(img, [pts], True, (0, 0, 255), 2)
+    return cv2.polylines(img, [pts], True, rgb, 2)
 
 
+def save_imgs_with_figure(num):
+    """"画像にアノテーションし（矩形や多角形を描写し）、保存する
+
+    Args:
+        num (int): 画像やjsonの番号
+    """
+    img = cv2.imread(train_images[num])
+    load_json = read_json(str(train_annotations[num]))
+    for item in load_json['labels']:
+        # doors
+        if item in rect_keys:
+            for pts in load_json['labels'][item]:
+                pts = np.array(pts, np.int)
+                draw_rectangle(img, pts)
+        # rooms
+        elif item in polygon_keys:
+            for pts in load_json['labels'][item]:
+                pts = np.array(pts, np.int32)
+                draw_polylines(img, pts)
+        else:
+            for pts in load_json['labels'][item]:
+                pts = np.array(pts, np.int32)
+                draw_polylines(img, pts, (0, 255, 0))
+    cv2.imwrite('/content/drive/MyDrive/Colab_Notebooks/kaggle/panasonic/'
+                'data/imgs_anno/' +
+                os.path.basename(train_images[num]), img)
+
+
+def convert_to_coco_format(json_file):
+    attrDict = dict()
+    attrDict["categories"] = [{"supercategory": "door", "id": 1,
+                               "name": "引戸"},
+                              {"supercategory": "door", "id": 2,
+                              "name": "折戸"},
+                              {"supercategory": "door", "id": 3,
+                                  "name": "開戸"},
+                              {"supercategory": "room", "id": 1,
+                              "name": "LDK"},
+                              {"supercategory": "room", "id": 2,
+                                  "name": "廊下"},
+                              {"supercategory": "room", "id": 3,
+                              "name": "浴室"},
+                              {"supercategory": "room", "id": 4,
+                              "name": "洋室"},
+                              ]
+    images = list()
+    annotations = list()
+    image_id = 0
+    for file in json_file:
+        image_id = image_id + 1
+        annotation_path = file
+        image = dict()
+        doc = xmltodict.parse(
+            open(annotation_path).read(), force_list=('object'))
+        image['file_name'] = str(doc['annotation']['filename'])
+        image['height'] = int(doc['annotation']['size']['height'])
+        image['width'] = int(doc['annotation']['size']['width'])
+        image['id'] = image_id
+        print("File Name: {} and image_id {}".format(file, image_id))
+        images.append(image)
+        id1 = 1
+        if 'object' in doc['annotation']:
+            for obj in doc['annotation']['object']:
+                for value in attrDict["categories"]:
+                    annotation = dict()
+                    if str(obj['name']) == value["name"]:
+                        annotation["iscrowd"] = 0
+                        annotation["image_id"] = image_id
+                        x1 = int(obj["bndbox"]["xmin"]) - 1
+                        y1 = int(obj["bndbox"]["ymin"]) - 1
+                        x2 = int(obj["bndbox"]["xmax"]) - x1
+                        y2 = int(obj["bndbox"]["ymax"]) - y1
+                        annotation["bbox"] = [x1, y1, x2, y2]
+                        annotation["area"] = float(x2 * y2)
+                        annotation["category_id"] = value["id"]
+                        annotation["ignore"] = 0
+                        annotation["id"] = id1
+                        annotation["segmentation"] = [
+                            [x1, y1, x1, (y1 + y2), (x1 + x2), (y1 + y2), (x1 + x2), y1]]
+                        id1 += 1
+                        annotations.append(annotation)
+
+            else:
+                print("File: {} doesn't have any object".format(file))
+
+        else:
+            print("File: {} not found".format(file))
+
+    attrDict["images"] = images
+    attrDict["annotations"] = annotations
+    attrDict["type"] = "instances"
+
+    jsonString = json.dumps(attrDict)
+    with open("train.json", "w") as f:
+        f.write(jsonString)
 
 
 if __name__ == '__main__':
-    import sys
+    pass
+    # import multiprocessing as multi
+    # from multiprocessing import Pool
 
-    print(sys.version)
+    # nums = len(train_images)
+    # # mulitiprocessing
+    # core = multi.cpu_count()
+    # print(core)
+    # p = Pool(core)
+    # imap = p.imap(save_imgs_with_figure, list(range(nums)))
+    # result = list(tqdm(imap, total=nums))
